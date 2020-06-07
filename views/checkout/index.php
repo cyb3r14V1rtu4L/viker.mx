f7ce9d6bbe1fad203f62d5c8b351b5077f774b9c
Cyberio L.0 <cybberio@gmail.com>
2020-06-07 16:03:44 -0500

    fixOrderPaypal.pt1

--- views/checkout/index.php
+++ views/checkout/index.php
@@ -16,8 +16,8 @@
                 <img class="img-responsive" width="32px" height="32px" src="/public/img/logo_open.png" alt="" /></div>
 
         </h2>
-        </div>
-        <!-- /.col -->
+    </div>
+    <!-- /.col -->
     </div>
 
     <div class="row">
@@ -42,36 +42,33 @@
                         </tr>
                         </thead>
                         <tbody>
-                            <?php
-                            $paypal = '';
-                            $currency = 'USD';
-                            foreach ($enterprise as $e_id => $stuff) {                      
-                                if ($e_id !== 'enterprise_data') {
-                                    $subtotal = 0;
-                                    foreach ($stuff as $s_id => $Stuff) {
-                                        #$this->pr($Stuff);
-                                        if(is_array($Stuff)) {
-                                            foreach($Stuff as $stuff)
-                                            {
-                                                ?>
-                                                <tr>
-                                                    <td><?php echo $stuff['how_many']; ?></td>
-                                                    <td><?php echo $stuff['stuff_data']['name_stuff']; ?></td>
-                                                    <td class="pull-right" >$<?php echo number_format($stuff['how_many']*$stuff['price'],2,'.',','); ?></td>
-                                                </tr>
-                                                <?php
-                                                    $subtotal +=($stuff['how_many']*$stuff['price']);
+                        <?php
+                        $subtotal = 0;
+                        foreach ($enterprise as $e_id => $stuff)
+                        {
+                            if ($e_id !== 'enterprise_data'){
+                                $subtotal = 0;
+                                foreach ($stuff as $s_id => $Stuff) {
+                                    #$this->pr($Stuff);
+                                    if(is_array($Stuff)) {
+                                        foreach($Stuff as $stuff)
+                                        {
+                                            ?>
+                                            <tr>
+                                                <td><?php echo $stuff['how_many']; ?></td>
+                                                <td><?php echo $stuff['stuff_data']['name_stuff']; ?></td>
+                                                <td class="pull-right" >$<?php echo number_format($stuff['how_many']*$stuff['price'],2,'.',','); ?></td>
+                                            </tr>
 
-                                            }
-                                            $_SESSION['CheckoutShop']['gran_total'] = $subtotal;
+                                            <?php
+                                            $subtotal +=($stuff['how_many']*$stuff['price']);
+
                                         }
                                     }
-                                } else {
-                                    $paypal = $stuff['paypal_account'];
-                                    $currency = $stuff['paypal_currency'];
                                 }
                             }
-                            ?>
+                        }
+                        ?>
                         </tbody>
                     </table>
 
@@ -93,73 +90,88 @@
 
 
     <!-- paypalPayment -->
-    <?php
-        $order = date('ymdHis');
-    ?>
+
     <div class="row p-bottom-40">
-
-        <div class="row" id="paypalPayment" style="display: none;">
-            <?php
-            $error = false;
-            $amount = 0;
-            $concept = '';
-
-            if (isset($_GET['error']))
-                $error = $_GET['error'];
-
-            if (isset($_GET['amount']))
-                $amount = $_GET['amount'];
-
-            if (isset($_POST['submitPayment'])) {
-                $amount = $_POST['amount'];
-                $concept = 'Food and Stuff Delivery';
-                ?>
-                <div class="loading">Please Wait...</div>
-                <script>
-                    $(document).ready(function () {
-                        $("#realizarPago").submit();
-                    });
-                </script>
-            <?php
-            } else {
-            ?>
-                <form class="form-amount" action="/checkout/index" method="post">
-                    <div class="form-group">
-                        <input type="hidden" id="concept" name="concept" class="form-control" value="<?php echo $concept; ?>">
-                    </div>
-                    <div class="form-group">
-                        <input type="hidden" id="amount" name="amount" class="form-control" value="<?php echo $amount; ?>">
-                    </div>
-                    <input class="btn btn-lg btn-primary btn-block" name="submitPayment" onclick="orderCheckout('paypal');" type="submit" value="Order NOW!!">
-
-                </form>
-                <?php
-            }
-            ?>
-
-        </div>
         <!-- accepted payments column -->
         <div class="col-xs-6">
-            <form id="realizarPago" action="https://www.paypal.com/cgi-bin/webscr" method="post">
-                <input name="cmd" type="hidden" value="_cart" />
-                <input name="upload" type="hidden" value="1" />
-                <input name="business" type="hidden" value="<?php echo $paypal; ?>" />
-                <input name="shopping_url" type="hidden" value="/" />
-                <input name="currency_code" type="hidden" value="<?php echo $currency; ?>" />
-                <input name="return" type="hidden" value="https://viker.mx/checkout/confirmation" />
-                <input name="notify_url" type="hidden" value="https://viker.mx/ipn.php" />
 
-                <input name="rm" type="hidden" value="2" />
-                <input name="item_number_1" type="hidden" value="<?php echo $order; ?>" />
-                <input name="item_name_1" type="hidden" value="<?php echo $concept; ?>" />
-                <input name="amount_1" type="hidden" value="<?php echo $amount; ?>" />
-                <input name="quantity_1" type="hidden" value="1" />
-            </form>
+            <!--<p class="lead">Payment Methods:</p>
+            <img src="../../dist/img/credit/visa.png" alt="Visa">
+            <img src="../../dist/img/credit/mastercard.png" alt="Mastercard">
+            <img src="../../dist/img/credit/american-express.png" alt="American Express">
+            <img src="../../dist/img/credit/paypal2.png" alt="Paypal">
+
+            <p class="text-muted well well-sm no-shadow" style="margin-top: 10px;">
+                Etsy doostang zoodles disqus groupon greplin oooj voxy zoodles, weebly ning heekya handango imeem plugg
+                dopplr jibjab, movity jajah plickers sifteo edmodo ifttt zimbra.
+            </p>-->
         </div>
         <!-- /.col -->
         <div class="col-xs-6">
 
+            <div class="row" id="paypalPayment" style="display: none;">
+                <?php
+                $error = false;
+                $amount = '';
+                $concept = '';
 
+                if (isset($_GET['error']))
+                    $error = $_GET['error'];
+
+                if (isset($_GET['amount']))
+                    $amount = $_GET['amount'];
+
+                if (isset($_POST['submitPayment'])) {
+
+                    $amount = $_POST['amount'];
+                    $concept = 'Food and Stuff Delivery';
+                    $order = date('ymdHis');
+
+                    ?>
+
+                    <div class="loading">Please Wait...</div>
+
+                    <form id="realizarPago" action="https://www.paypal.com/cgi-bin/webscr" method="post">
+                        <input name="cmd" type="hidden" value="_cart" />
+                        <input name="upload" type="hidden" value="1" />
+                        <input name="business" type="hidden" value="cyberia.virtual@gmail.com" />
+                        <input name="shopping_url" type="hidden" value="/" />
+                        <input name="currency_code" type="hidden" value="MXN" />
+                        <input name="return" type="hidden" value="https://viker.mx/checkout/confirmation" />
+                        <input name="notify_url" type="hidden" value="https://viker.mx/ipn.php" />
+
+                        <input name="rm" type="hidden" value="2" />
+                        <input name="item_number_1" type="hidden" value="<?php echo $order; ?>" />
+                        <input name="item_name_1" type="hidden" value="<?php echo $concept; ?>" />
+                        <input name="amount_1" type="hidden" value="<?php echo $amount; ?>" />
+                        <input name="quantity_1" type="hidden" value="1" />
+
+                    </form>
+                    <script>
+                        $(document).ready(function () {
+                            $("#realizarPago").submit();
+                        });
+                    </script>
+
+                <?php
+                }
+                else {
+                ?>
+                    <form class="form-amount" action="/checkout/index" method="post">
+                        <div class="form-group">
+                            <input type="hidden" id="concept" name="concept" class="form-control" value="<?php echo $concept; ?>">
+                        </div>
+                        <div class="form-group">
+                            <input type="hidden" id="amount" name="amount" class="form-control" value="1.00">
+                        </div>
+                        <input class="btn btn-lg btn-primary btn-block" name="submitPayment" onclick="orderCheckout('paypal');" type="submit" value="Order NOW!!">
+
+                    </form>
+                    <?php
+                }
+                ?>
+
+            </div>
 
         </div>
         <!-- /.col -->
@@ -167,14 +179,14 @@
     <!-- /.row -->
 
     <!-- this row will not appear when printing -->
-   <!-- <div class="row no-print">
-        <div class="col-xs-12">
-            <a href="invoice-print.html" target="_blank" class="btn btn-default"><i class="fa fa-print"></i> Print</a>
-            <button type="button" class="btn btn-success pull-right"><i class="fa fa-credit-card"></i> Submit Payment
-            </button>
-            <button type="button" class="btn btn-primary pull-right" style="margin-right: 5px;">
-                <i class="fa fa-download"></i> Generate PDF
-            </button>
-        </div>
-    </div>-->
+    <!-- <div class="row no-print">
+         <div class="col-xs-12">
+             <a href="invoice-print.html" target="_blank" class="btn btn-default"><i class="fa fa-print"></i> Print</a>
+             <button type="button" class="btn btn-success pull-right"><i class="fa fa-credit-card"></i> Submit Payment
+             </button>
+             <button type="button" class="btn btn-primary pull-right" style="margin-right: 5px;">
+                 <i class="fa fa-download"></i> Generate PDF
+             </button>
+         </div>
+     </div>-->
 </section>
